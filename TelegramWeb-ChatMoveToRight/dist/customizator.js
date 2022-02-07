14f0ae27a43bc8d643851918ccd9701a9ef95fae
ShizzaHo <chekan898@gmail.com>
2022-02-07 14:12:58 +0500

    первый залив

---
+++ TelegramWeb-ChatMoveToRight/dist/customizator.js
@@ -0,0 +1,28 @@
+let chats = document.querySelectorAll("div.ListItem");
+refactor()
+toLeft()
+
+function chatclick() {
+    document.getElementsByClassName("messages-container")[0].onDOMContentLoaded = toLeft();
+}
+
+function toLeft() {
+    document.getElementsByClassName("messages-container")[0].style.margin = "0";
+
+    let flexy = document.getElementsByClassName("Transition__slide--active")
+
+    for (let i = 0; i < flexy.length; i++) {
+        const element = flexy[i];
+        element.style.alignItems = "flex-start";
+    }
+
+    chats = document.querySelectorAll("div.ListItem");
+    refactor()
+}
+
+function refactor() {
+    for (let i = 0; i < chats.length; i++) {
+        const element = chats[i];
+        element.addEventListener("click", chatclick);
+    }
+}
